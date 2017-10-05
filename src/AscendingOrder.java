4ac293750a37b0d6153624f9d06701371eada07e
melissa.forfitt <melissa.forfitt@10.4.192.255>
2017-10-05 15:18:33 +0100

    Created 3 inputs and attempted sorting algorithm.

--- src/AscendingOrder.java
+++ src/AscendingOrder.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 
 public class AscendingOrder {
 
 	public static void main(String[] args) {
 		
-
+		int x;
+		int y;
+		int z;
+		int temp;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Input first number.");
+		x = scan.nextInt();
+		System.out.println("Input second number.");
+		y = scan.nextInt();
+		System.out.println("Input third number.");
+		z = scan.nextInt();
+		scan.close();
+		
+		if (x > y) {
+			temp = x;
+			y = z;
+			z = y;
+		}
+		
+		System.out.println(x & y & z);
+		
 	}
 
 }
